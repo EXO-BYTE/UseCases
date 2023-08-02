bae4f3d1a44448d097d26de3583e8b7600b95e30
Aishwarya S <83657227+EXO-BYTE@users.noreply.github.com>
2023-08-02 14:08:01 +0530

    Update Application.java

--- src/usecase/Application.java
+++ src/usecase/Application.java
@@ -1,5 +1,29 @@
 package usecase;
 
 public class Application {
+      public static void main(String[] args) {
+       
+        User user1 = new User(1, "Aish");
+        User user2 = new User(2, "Alice");
+        
+        
+        Book book1 = new Book(1, "Java Basics");
+        Book book2 = new Book(2, "DBMS");
+        
+       
+        Order order1 = new Order(901, user1);
+        Order order2 = new Order(902, user2);
+        
+     
+        order1.addBook(book1);
+        order2.addBook(book2);
+        
+       
+        Payment payment1 = new Payment(5001, order1, 250);
+        Payment payment2 = new Payment(5002, order2, 199);
+        
+        
+    }
+
 
 }
